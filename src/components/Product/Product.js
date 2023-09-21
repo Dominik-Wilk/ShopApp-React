2309648f565f5807942cd54953505bcb9011edf1
Dominik Wilk <mr.wilkd@gmail.com>
2023-09-21 15:02:12 +0100

    Create core of the website app

---
+++ src/components/Product/Product.js
@@ -0,0 +1,72 @@
+import styles from './Product.module.scss';
+import clsx from 'clsx';
+import Button from '../Button/Button';
+import { useState } from 'react';
+import { nanoid } from 'nanoid'; // import propTypes from "prop-types"
+
+const Product = ({ colors, sizes, name, title, basePrice }) => {
+  const [currentColor, setCurrentColor] = useState(colors[0]);
+  const [currentSize, setCurrentSize] = useState(sizes[0].name);
+
+  const prepareColorClassName = color => {
+    return styles['color' + color[0].toUpperCase() + color.substr(1).toLowerCase()];
+  };
+  return (
+    <article className={styles.product}>
+      <div className={styles.imageContainer}>
+        <img
+          className={styles.image}
+          alt={title}
+          src={`${process.env.PUBLIC_URL}/images/products/shirt-${name}--${currentColor}.jpg`}
+        />
+      </div>
+      <div>
+        <header>
+          <h2 className={styles.name}>{title}</h2>
+          <span className={styles.price}>Price: {basePrice}$</span>
+        </header>
+        <form>
+          <div className={styles.sizes}>
+            <h3 className={styles.optionLabel}>Sizes</h3>
+            <ul className={styles.choices}>
+              {sizes.map(size => (
+                <li key={nanoid()}>
+                  <button type='button' className={clsx(size.name === currentSize && styles.active)}>
+                    {size.name}
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className={styles.colors}>
+            <h3 className={styles.optionLabel}>Colors</h3>
+            <ul className={styles.choices}>
+              {colors.map(item => (
+                <li key={item}>
+                  <button
+                    type='button'
+                    className={clsx(prepareColorClassName(item), item === currentColor && styles.active)}
+                  />
+                </li>
+              ))}
+              {/* <li>
+                <button type='button' className={clsx(styles.colorBlack, styles.active)} />
+              </li>
+              <li>
+                <button type='button' className={clsx(styles.colorRed)} />
+              </li>
+              <li>
+                <button type='button' className={clsx(styles.colorWhite)} />
+              </li> */}
+            </ul>
+          </div>
+          <Button className={styles.button}>
+            <span className='fa fa-shopping-cart' />
+          </Button>
+        </form>
+      </div>
+    </article>
+  );
+};
+
+export default Product;
